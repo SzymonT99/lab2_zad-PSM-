fe5000f381aabaa554307b845e64b6298aad39f0
Szymon Tyrka <szy.tyrka@gmail.com>
2020-10-10 18:23:14 +0200

    change in app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const _ = require("lodash");
 
-function average(){
+function average(array){
 	let result = 0;
-	result = _.mean(arguments);
+	result = _.mean(array);
 	
 	console.log("Srednia wynosi: " + result);
 	
 }
 
-average(1,2,3,4,5,6);
+average([1,2,3,4,5,6]);
+
