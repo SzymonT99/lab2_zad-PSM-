e3533fed021fbca5424b87738e6db4c1129bdccc
Szymon Tyrka <szy.tyrka@gmail.com>
2020-10-10 18:33:55 +0200

    Add min and max in app.js

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const _ = require("lodash");
 
-function average(array){
+function calculations(array){
 	let result = 0;
 	result = _.mean(array);
 	
+	let max = _.max(array);
+	let min = _.min(array);
+	
 	console.log("Srednia wynosi: " + result);
+	console.log("Największa wartosc elementu w tablicy wynosi: " + max);
+	console.log("Najmniejsza wartosc elementu w tablicy wynosi: " + min);
 	
 }
 
-average([1,2,3,4,5,6]);
+calculations([1,2,3,4,5,6]);
 
