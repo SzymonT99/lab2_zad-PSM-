49b811de6e952b84a8f95df1e77555faf27bac6b
Szymon Tyrka <szy.tyrka@gmail.com>
2020-10-11 00:17:27 +0200

    Add function for object user

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// zad 1 i 2
 const _ = require("lodash");
 
 function calculations(array){
@@ -15,3 +16,54 @@ function calculations(array){
 
 calculations([1,2,3,4,5,6]);
 
+
+// zad 3
+const user = {
+	name: 'Imie',
+	surname: 'Nazwisko',
+	allGrades: [
+		{
+			subjectName: 'Name1',
+			grades: [5, 4, 3, 5, 2],
+			weight: 3
+		},
+		{
+			subjectName: 'Name2',
+			grades: [3, 3.5, 2],
+			weight: 1
+		},
+		{
+			subjectName: 'Name3',
+			grades: [4, 3, 3.5],
+			weight: 5
+		}
+	]
+}
+
+function  checkUser(user){
+	
+	let sum = 0;
+	let sumOfWeight = 0;
+	let average = 0;
+	
+	for (let i = 0; i < user.allGrades.length; i++){
+		
+		sumOfWeight += user.allGrades[i].weight * user.allGrades[i].grades.length;
+		
+		for (let j = 0; j < user.allGrades[i].grades.length; j++){
+			
+			let currentGrade = user.allGrades[i].grades[j];
+			let currentWeight = user.allGrades[i].weight;
+			sum += (currentGrade * currentWeight);
+			
+		}
+	}
+	
+	average = sum / sumOfWeight;
+	average = Math.round(average * 100) / 100;
+	
+	console.log("Użytkownik o imieniu: " + user.name + " oraz nazwisku: " + user.surname + " osiągną średnią ważoną: " + average);
+	
+}
+
+checkUser(user);
